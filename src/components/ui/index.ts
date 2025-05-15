801892c57f8be255ede8851594b1fc702cd10c24
Andrew Caldas <contandrewcaldas@gmail.com>
2025-05-15 04:00:36 -0300

    feat: add new components shadcn ui

---
+++ src/components/ui/index.ts
@@ -0,0 +1,7 @@
+export * from './input';
+export * from './label';
+export * from './select';
+export * from './slider';
+export * from './form';
+export * from './textarea';
+export * from './button';
